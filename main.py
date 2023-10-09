ccf7b9f4090ef12ad052bf4c5282ca677188f2d5
Art Gilyazov <art.gilyazov@mail.ru>
2023-10-09 14:40:28 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import os
 import requests
 
 
-url = 'https://tululu.org/txt.php?id=32168'
+filename_template = 'id'
+directory = 'books'
+url_template = 'https://tululu.org/txt.php?id='
 
-response = requests.get(url)
-response.raise_for_status() 
 
-filename = 'book.txt'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+def save_image(book, directory, filename_template, number_of_book):
+    with open(f'{directory}/{filename_template}{number_of_book}.txt', 'wb') as file:
+        file.write(book)
+
+
+def get_book_from_site():
+    os.makedirs(directory, exist_ok=True)
+
+    for number_of_book in range(1, 11):
+        url = f'{url_template}{number_of_book}'
+        response = requests.get(url)
+        response.raise_for_status() 
+
+        save_image(response.content, directory, filename_template, number_of_book)
+
+if __name__ == '__main__':
+    get_book_from_site()
