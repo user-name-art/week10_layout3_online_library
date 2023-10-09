c46d43e848589962e71a41a3290c8a3c8ac01580
Art Gilyazov <art.gilyazov@mail.ru>
2023-10-09 15:41:00 +0300

    add redirect processing

--- main.py
+++ main.py
@@ -12,15 +12,26 @@ def save_image(book, directory, filename_template, number_of_book):
         file.write(book)
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+
 def get_book_from_site():
     os.makedirs(directory, exist_ok=True)
 
     for number_of_book in range(1, 11):
         url = f'{url_template}{number_of_book}'
         response = requests.get(url)
-        response.raise_for_status() 
+        response.raise_for_status()
 
-        save_image(response.content, directory, filename_template, number_of_book)
+        try:
+            check_for_redirect(response)
+            save_image(response.content, directory, filename_template, number_of_book)
+        except requests.exceptions.HTTPError:
+            print(f'По ссылке {url} нет книги для скачивания.')
+
 
 if __name__ == '__main__':
     get_book_from_site()
